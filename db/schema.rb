d88c5d9960f19d594f26f4fd64322413cc5c1b47
shijimi-0530 <hatta@anconsulting.jp>
2023-01-28 02:03:08 +0000

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_24_200133) do
+ActiveRecord::Schema.define(version: 2023_01_26_062647) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string "major_category_name"
-    t.string "name"
-    t.text "description"
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.string "nickname", null: false
+    t.integer "gender", null: false
+    t.date "birth_date", null: false
+    t.string "avatar_image", null: false
+    t.boolean "admin", default: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
